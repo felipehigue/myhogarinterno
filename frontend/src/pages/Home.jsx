3f06449236c4448410ba57d18aa14ef11634871e
felipe <felipehiguera27@gmail.com>
2024-12-14 09:33:56 -0500

    estilos basicos

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Mi Hogar Interno</h1>
-      <div>
-        <Link to="/">
+    <div className="container mx-auto p-4 mt-4">
+      <h1 className="text-3xl font-bold mb-4 text-center text-black uppercase">MI HOGAR INTERNO</h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>Inicio</button>
         </Link>
-        <Link to="/edit/esculpir">
+        <Link to="/edit/esculpir" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>esculpir</button>
         </Link>
-        <Link to="/edit/ser">
+        <Link to="/edit/ser" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>ser</button>
         </Link>
-        <Link to="/edit/dejarhuella">
+        <Link to="/edit/dejarhuella" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>dejar huella</button>
         </Link>
-        <Link to="/edit/mihogarmundo">
+        <Link to="/edit/mihogarmundo" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>mi hogar y mi mundo</button>
         </Link>
-        <Link to="/edit/aventura">
+        <Link to="/edit/aventura" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>aventura</button>
         </Link>
-        <Link to="/edit/granbiblioteca">
+        <Link to="/edit/granbiblioteca" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <button>gran biblioteca</button>
         </Link>
       </div>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Procesa todos los componentes dentro de `src`
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
